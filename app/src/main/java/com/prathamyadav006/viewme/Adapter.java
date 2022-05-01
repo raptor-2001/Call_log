3e0b15abccb5a332186d44a28dc29e56302d228f
Pratham Yadav <pratham007yadav@gmail.com>
2022-05-01 17:29:59 +0530

    first commit

--- app/src/main/java/com/prathamyadav006/viewme/Adapter.java
+++ app/src/main/java/com/prathamyadav006/viewme/Adapter.java
@@ -1,7 +1,9 @@
 package com.prathamyadav006.viewme;
 
 import android.content.Context;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -12,6 +14,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
+import java.util.zip.Inflater;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
@@ -22,7 +25,8 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         this.userlist = userlist;
     };
 
-
+  //public static class ViewHolder extends RecyclerView.ViewHolder
+  //        implements View.OnCreateContextMenuListener
     @NonNull
     @Override
     public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -38,12 +42,14 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
         holder.setData(image,animalName);
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(context, "This is "+ animalName, Toast.LENGTH_SHORT).show();
+            public boolean onLongClick(View v) {
+                return false;
             }
         });
+
+
     }
 
 
@@ -52,7 +58,8 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
         return userlist.size();
     }
 
-    public class ViewHolder extends  RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder
+            implements View.OnCreateContextMenuListener {
 
         ImageView imageview;
         TextView textview;
@@ -61,13 +68,19 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
             imageview = itemView.findViewById(R.id.imageview);
             textview = itemView.findViewById(R.id.textview);
-
+            itemView.setOnCreateContextMenuListener(this);
         }
 
         public void setData(int image, String animalName) {
 
             imageview.setImageResource(image);
             textview.setText(animalName);
+
+        }
+
+
+        @Override
+        public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
 
         }
     }
--- app/src/main/java/com/prathamyadav006/viewme/MainActivity.java
+++ app/src/main/java/com/prathamyadav006/viewme/MainActivity.java
@@ -1,11 +1,17 @@
 package com.prathamyadav006.viewme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,21 +28,28 @@ public class MainActivity extends AppCompatActivity {
 
         initdata();
         initRecycler();
+        registerForContextMenu(recyclerView);
     }
 
     private void initdata() {
 
         userList = new ArrayList<>();
 
-        userList.add(new ModelClass(R.drawable.tiget,"Tiger"));
-        userList.add(new ModelClass(R.drawable.lion,"Lion"));
-        userList.add(new ModelClass(R.drawable.eagle,"Eagle"));
-        userList.add(new ModelClass(R.drawable.elephant,"Elephant"));
-        userList.add(new ModelClass(R.drawable.monkey,"Monkey"));
-        userList.add(new ModelClass(R.drawable.panda,"Panda"));
-        userList.add(new ModelClass(R.drawable.parrot,"Parrot"));
-        userList.add(new ModelClass(R.drawable.rabbit,"Rabbit"));
-        userList.add(new ModelClass(R.drawable.wolf,"Wolf"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Pratham IT"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Mahesh CSE"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Sarthak IT"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Ankur IT"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Sudesh EN"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Mohit EN"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Kartik CSE -1"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Aaditya Patil IT -1"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Coordinator Mr. A"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Abhishek IT"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Mohit IT"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Mayuresh IT"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Hrishikesh IT"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Ashish IT"));
+        userList.add(new ModelClass(R.drawable.ic_profile,"Onkar IT +1"));
 
     }
 
@@ -50,5 +63,37 @@ public class MainActivity extends AppCompatActivity {
         adapter = new Adapter(userList,this);
         recyclerView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        //menu.setHeaderTitle("Select Action");
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.call){
+            Toast.makeText(this, "call selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }else if(item.getItemId() == R.id.copy){
+            Toast.makeText(this, "copy selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }else if(item.getItemId() == R.id.sim1){
+            Toast.makeText(this, "SIM 1 selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }else if(item.getItemId() == R.id.sim2){
+            Toast.makeText(this, "SIM 2 selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }else if(item.getItemId() == R.id.edit){
+            Toast.makeText(this, "Edit selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }else if(item.getItemId() == R.id.delete){
+            Toast.makeText(this, "Delete selected", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return true;
     }
 }
